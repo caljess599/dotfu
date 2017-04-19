12ca7e68952558f3b7cf693c67bcf711b67c88ae
Jessica Otey <jotey@nrao.edu>
2017-04-19 12:12:22 -0400

    first deployment of SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -10,15 +10,16 @@
 # ----------------------------------------------------------------------------
 #  18 Apr 2017   |  jotey    | started this changelog! 
 # ----------------------------------------------------------------------------
+#  19 Apr 2017   |  jotey    | first deployment, afrec generic only, no rcs 
 # ----------------------------------------------------------------------------
 # ----------------------------------------------------------------------------
 # ----------------------------------------------------------------------------
 
 # # # USAGE FROM TEMPLATE # # #
 
-USAGE_MESSAGE="[This script takes no args]"
+USAGE_MESSAGE="[name of alternative file (default is ~/.bashrc)]"
 ARGMIN=0
-ARGMAX=0
+ARGMAX=1
 
 usage () {
 	printf "usage: %s $USAGE_MESSAGE \n" $0 >&2
@@ -36,23 +37,32 @@ if [ $# -gt $ARGMAX ]; then
 fi
 
 # # # FUNCTIONS # # #
+isrcs(){
+	# accept a file name
+	givenname=$1
+	# where it is
+	pathtofile=$(dirname $1)
+	# what it's called
+	basename=$(basename $1)
+	# preserve check on its existence
+	if [ ! -e $1 ]; then
+		echo "No such file exists"
+	else
+		if
+			[ -e ${pathtofile}/RCS/${basename},v ]; then
+			return 1
+		else
+			return 0
+		fi
+	fi
+}
 
-# # # VARIABLES # # #
-# tip: relative paths are your enemy! #
-# logpath=/absolute/path/to/relevant/dir
+addaf(){
+	cat << 'EOF' >> $BDIR/$BASHRC
 
-# # # MAIN PROGRAM # #
-cd /$USER && ls dotfu >/dev/null && echo "...dotfu repo found, continuing"
-
-grep -q "af(){"
-if [ $? -eq 0 ]; then
-	echo "af function already installed, quitting
-	exit
-else
-	echo ...testing to see if /$USER/.bashrc is under RCS control...
-	# TK
-	echo ...installing af function
-	cat << EOF >> /$USER/.bashrc
+# af function for dotfu repo
+# usage: af [-v] <dotfu folder> [...]
+# -v flag produces verbose output
 af(){
 	repo=/$USER/dotfu
 	if [ "$1" = "-v" ]; then
@@ -73,13 +83,59 @@ af(){
 		done
 	fi
 }
-EOF
 
-# Section for afrec
-# echo these are the dotfu categories
-# echo type in, with spaces, the ones you would like to source via afrec
-# echo or leave blank to accept generic recommendation (math screen rcs)
-# that functionality...
-# add the afrec function to .bashrc
+# generic afrec options, with verbose option
+# use eval $(afrec) to load
+afrec(){
+	echo af -v math megaraid screen rcs
+}
+EOF
+}
+
+# # # VARIABLES # # #
+# tip: relative paths are your enemy! #
+
+# if alternative file name provided, use it
+if [ $# -gt $ARGMIN ]; then
+	BASHRC=$1
+else
+	BASHRC=".bashrc"
 fi
 
+# # # MAIN PROGRAM # #
+PDIR=$(pwd)
+if [ ${PDIR##*\/} != dotfu ]; then
+	echo "Please run this script from dotfu directory"
+	exit
+fi
+
+BDIR=${PDIR%*\/dotfu}
+echo ... using $BDIR for $BASHRC path...
+
+# see if function exists, uncommented
+grep -q "^af(){" $BDIR/${BASHRC} 
+
+# may not need to install
+if [ $? -eq 0 ]; then
+	echo "af function already installed, quitting"
+	exit
+else
+	echo ... testing to see if $BDIR/$BASHRC is under RCS control...
+	isrcs $BDIR/$BASHRC
+	if [ $? -eq 1 ]; then
+		echo ... unlocking $BDIR/$BASHRC...
+	       # unlock the file
+	       	echo ... installing af function to $BDIR/$BASHRC...
+		addaf
+		echo ... locking file...
+		# lock the file w/comment
+	else
+		echo ... this file is not under RCS, continuing...
+		echo ... installing af function to $BDIR/$BASHRC ...
+		addaf
+	
+	fi
+	echo ... IF THIS IS A PERSONAL BOX, you will need to adjust the repo path ...
+	echo ... afrec installed with default options: math megaraid screen rcs ...
+	echo ... feel free to edit based on the needs of this box ...
+fi
